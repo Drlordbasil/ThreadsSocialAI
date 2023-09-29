a3f96b3fd11f02eb4b43753daa7fd464bc647cca
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-29 07:57:37 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    """
+    Configuration class for the influencer bot.
+    """
+    THREADS_NET_USERNAME = 'your_username'
+    THREADS_NET_PASSWORD = 'your_password'
+    GPT4_MODEL_PATH = 'orca-mini-3b.ggmlv3.q4_0.bin'
