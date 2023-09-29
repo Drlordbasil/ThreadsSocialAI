51486d8f04d64e15508c4843efa5c3630bae32e2
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-29 07:56:23 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
-# bot.py
-from gpt4all_setup import setup_gpt4all
-from chrome_setup import setup_chrome_driver
-from threads_interactions import login
+# Update bot.py
+
+from content_generator import generate_post_title_and_content, generate_reply
+from threads_interactions import post_content, reply_to_comment
 
 def main():
     """
     Main function to orchestrate the influencer bot activities.
     """
-    model = setup_gpt4all("orca-mini-3b.ggmlv3.q4_0.bin")
     driver = setup_chrome_driver()
     login(driver, 'your_username', 'your_password')
+    
+    # Post new content
+    title, content = generate_post_title_and_content()
+    post_content(driver, title, content)
+    
+    # Assume we have a function to get comments and their ids
+    comments = get_comments(driver, 'post_url_here')
+    for comment_id, comment_text in comments:
+        reply_text = generate_reply(comment_text)
+        reply_to_comment(driver, 'post_url_here', comment_id, reply_text)
+    
     # Additional logic for social media influencing on Threads.net
 
 if __name__ == "__main__":
